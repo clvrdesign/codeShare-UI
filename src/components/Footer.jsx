cf020d6f97d2d6cf58dd35fe879eab4de246730b
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-08-19 00:29:35 +0200

    "Update footer text to remove link to Clever Designer and add Levi Okoye's name"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <div className="bg-gray-950">
         <p className="max-w-[1200px] m-auto flex items-center justify-center text-sm text-gray-600 gap-2 text-center border-t border-gray-900 py-5 px-3">
-            &copy; {year} codeShare | Levi,s Code Craft by<Link className="inline hover:text-[#f8f296] text-gray-500 ease-out duration-150" to='https://www.klevadev.com'>Clever Designer</Link>
+            &copy; {year} codeShare | Levi Okoye,s Challenge 
         </p>
         </div>
     )
