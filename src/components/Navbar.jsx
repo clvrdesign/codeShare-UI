21ea8e26a7a9b8128c27d7a7798d8f2bba71ec0d
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-08-19 00:46:01 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = ({createPost}) => {
   return (
     <nav className="sticky top-0 left-0 py-5 px-3 shadow-sm bg-[#f8f296] lg:border-none border-b border-[#ece890] z-40">
         <div className="max-w-[1200px] m-auto flex justify-between items-center">
-            <Link to='/posts' className="w-full h-[35px]">
+            <Link to='/' className="w-full h-[35px]">
                 <img className="h-full cursor-pointer md:block hidden" src={assets.logo} alt="" />
                 <img className="h-full cursor-pointer md:hidden block" src={assets.logo_small} alt="" />
             </Link>
             <ul className="flex items-center justify-between text-gray-900 lg:gap-10 gap-4 text-lg font-medium">
-                <Link to='/posts' className="w-fit cursor-pointer">
+                <Link to='/' className="w-fit cursor-pointer">
                     Posts
                 </Link>
                 <li onClick={createPost} className="w-fit cursor-pointer flex items-center">
